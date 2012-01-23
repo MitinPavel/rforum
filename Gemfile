4e105e5016a19105dc83e72768c7924d69ab059e
Andrei Bulava <abulava@gmail.com>
2012-01-23 11:34:08 +0200

    Bare Rails 3.0.9 application with BDD environment.

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'pg', '0.12.2'
+gem 'will_paginate', '3.0.pre2'
+
+group :development do
+  gem 'rspec-rails', '2.6.1'
+  gem 'annotate', '2.4.0'
+  gem 'faker', '0.3.1'
+  gem 'cucumber-rails', '1.2.1'
+end
+
+group :test do
+  gem 'rspec-rails', '2.6.1'
+  gem 'cucumber-rails', '1.2.1'
+  gem 'factory_girl_rails', '1.0'
+  gem 'capybara', '1.1.2'
+  gem 'database_cleaner', '0.7.1'
+  gem 'launchy', '2.0.5'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rforum::Application.config.secret_token = '3e502643686e088dc2fac10c5f8bed283edb47d3adf95653d6f275fea5648de9acf4b5d8cbafab9ec07db8a5a742c85159c95f6e7731b9eb1d754fe658c4e8ef'
